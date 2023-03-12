d2405a2071cbdc1a6c0e6a2ab74b34fc346b3a0e
Yudith Serrano <ycsa91@gmail.com>
2023-03-12 16:52:12 -0400

    Change tailwind to bootstrap and login page design

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,39 +8,37 @@
         <title>{{ config('app.name', 'Laravel') }}</title>
 
         <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @vite(['resources/css/app.css'])
+        <link href="https://fonts.googleapis.com/css?family=Nunito:400,600,700" rel="stylesheet">
 
         <!-- Styles -->
+        {{-- <link rel="stylesheet" href="{{ mix('css/app.css') }}"> --}}
+
         @livewireStyles
+
+        <!-- Scripts -->
+        {{-- <script src="{{ mix('js/app.js') }}" defer></script> --}}
+        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
     </head>
-    <body class="font-sans antialiased">
+    <body class="font-sans antialiased bg-light">
         <x-banner />
+        @livewire('navigation-menu')
 
-        <div class="min-h-screen bg-gray-100">
-            @livewire('navigation-menu')
+        <!-- Page Heading -->
+        <header class="d-flex py-3 bg-white shadow-sm border-bottom">
+            <div class="container">
+                {{ $header }}
+            </div>
+        </header>
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
-
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+        <!-- Page Content -->
+        <main class="container my-5">
+            {{ $slot }}
+        </main>
 
         @stack('modals')
 
         @livewireScripts
+
+        {{-- @stack('scripts') --}}
     </body>
 </html>
