874e8f13e94b9ba7b62d6dc0a5fa580ac1dc105b
Yudith Serrano <ycsa91@gmail.com>
2023-05-17 13:36:57 -0400

    Dashboard patient view

---
+++ resources/views/components/welcome-patient.blade.php
@@ -0,0 +1,27 @@
+<div>
+  <div class="card shadow container mb-4">
+      <div class="card-body">
+          <h1>Hello {{ 1 }}</h1>
+
+          <h2>Welcome to clinic Hope Valencia</h2>
+
+          <div class="text-center">
+              <button class="btn btn-primary btn-lg my-5  mx-5" type="button">Book Appointment</button>
+              <button class="btn btn-secondary btn-lg my-5  mx-5" type="button">Update Profile</button>
+          </div>
+          <div class="text-center">
+              <button class="btn btn-success btn-lg my-5  mx-5 text-white" type="button">New Appointment</button>
+          </div>
+
+          <div class="d-flex">
+              <div class="flex-grow-1">
+                  <h4>Doctor: {{ 2 }} Freddy 1/20/2023 at 4:pm</h4>
+              </div>
+              <div class="flex-grow-1 "><button class="btn btn-dark btn-lg text-white text-right">Cancel
+                      Appointment</button>
+              </div>
+          </div>
+
+      </div>
+  </div>
+</div>
