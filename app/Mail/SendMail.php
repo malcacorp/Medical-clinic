c195f94be6c501407002a30593fd1db39687525e
Yudith Serrano <ycsa91@gmail.com>
2023-05-23 11:44:43 -0400

    Welcome email

---
+++ app/Mail/SendMail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+// use Illuminate\Mail\Mailables\Content;
+// use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class SendMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $details;
+
+    /**
+     * Create a new message instance.
+     *
+     * @return void
+     */
+    public function __construct($details)
+    {
+      $this->details = $details;
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array
+     */
+    public function attachments()
+    {
+        return [];
+    }
+
+    public function build(){
+      return $this->from('sanctuary@sai.ngo')
+                  ->subject($this->details['subject'])
+                  ->view('emails.welcome', ['details' => $this->details]);
+    }
+}
