241e197ba0c926af297a2db6b46e18c8d6913ada
Yudith Serrano <ycsa91@gmail.com>
2023-05-24 22:58:35 -0400

    Appointment reminder emails

---
+++ app/Console/Commands/AppointmentReminder.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\Appointment;
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Mail\Markdown;
+use App\Mail\SendMail;
+
+class AppointmentReminder extends Command
+{
+  /**
+   * The name and signature of the console command.
+   *
+   * @var string
+   */
+  protected $signature = 'appointment:reminder';
+
+  /**
+   * The console command description.
+   *
+   * @var string
+   */
+  protected $description = 'Send an appointment reminder for tomorrow';
+
+  /**
+   * Execute the console command.
+   *
+   * @return int
+   */
+  public function handle()
+  {
+    $today = \Carbon\Carbon::now();
+    $today->addDay();
+    $tomorrow = $today->format('Y-m-d');
+    $appointments = Appointment::where("status", "pending")
+      ->where("date", $tomorrow);
+
+    foreach ($appointments as $appointment) {
+      $message = Markdown::parse(nl2br("Hola, " . $appointment->patient->first_name . ".\n\n Recuerda que tu cita médica con el Dr. (Dra.) " . $appointment->employee->first_name . " " . $appointment->employee->last_name . " será el día " . $appointment->date . " a las " . $appointment->time . ". \n\n Si necesitas cancelar tu cita, puedes hacer click en el enlace abajo. \n\n [Ir a Clinic Software](https://malcamedia.com) "));
+
+      $details = [
+        'title' => "Recordatorio de Cita Médica",
+        'subject' => "Recordatorio de Cita Médica",
+        'message' => $message,
+      ];
+
+      Mail::to([$appointment->patient->email])->send(new SendMail($details));
+    }
+
+    \Log::info('El comando AppointmentReminder se ejecutó correctamente.');
+    return Command::SUCCESS;
+  }
+}
